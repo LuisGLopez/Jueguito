e4f058864b5cbc24119332f00779527bcfba971e
LGLopez <DARKdestruktion@gmail.com>
2020-10-17 19:01:43 -0500

    Personaje animacion Idle (reacomodar codigo)

--- js/game.js
+++ js/game.js
@@ -1,24 +1,50 @@
 // Creacion de escena
 let gameScene = new Phaser.Scene('Jueguito');
-
+let player;
+let bg;
 // cargar assets
 gameScene.preload = function() {
     // Cargar las imagenes
-    this.load.image('background', 'assets/backgrounds/Purple.png')
-    this.load.image('player', 'assets/player/Jump (32x32).png')
+    this.load.image('background', 'assets/backgrounds/background.png')
+    this.load.spritesheet('player', 'assets/player/Idle (32x32).png', { frameWidth: 32, frameHeight: 32});
 };
 
 gameScene.create = function() {
+    let gameW = this.sys.game.config.width;
+    let gameH = this.sys.game.config.height;
+    
     // crear el backfground sprite
-    this.add.sprite(0, 0, 'background');
+    bg = this.add.sprite(0, 0, 'background');
+    bg.setPosition(gameW/2, gameH/2);
+
+    player = this.physics.add.sprite(50, 180, 'player');
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'turn',
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 10 }),
+        frameRate: 20,
+        repeat: -1
+    });
 };
+
+gameScene.update = function() {
+    player.anims.play('turn', true);
+}
 
 // Configuracion del juego
 let config = {
     type: Phaser.AUTO,
-    width:  1136,
-    height: 640,
-    scene: gameScene
+    width:  700,
+    height: 400,
+    scene: gameScene,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    }
 };
 
 // Creación de juego con la configuracion
